7a4ef23789c781869ff89ca8d558d25627446d53
sbmalik <malik@sigrow.com>
2026-05-25 22:31:28 +0200

    feat(chatbot): SSE converted to POST

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from collections.abc import AsyncIterable
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import EventSourceResponse, FileResponse
+from fastapi.responses import FileResponse
+from fastapi.sse import EventSourceResponse, ServerSentEvent
 from loguru import logger
 from pydantic import BaseModel
 from config.config import config
@@ -21,9 +22,8 @@ app.add_middleware(
 )
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None
+class UserMessage(BaseModel):
+    message: str
 
 
 STATIC_DIR = Path(__file__).parent / "static"
@@ -34,17 +34,19 @@ async def index() -> FileResponse:
     return FileResponse(STATIC_DIR / "index.html")
 
 
-@app.get("/items/stream", response_class=EventSourceResponse)
-async def sse_items(user_message: str) -> AsyncIterable[Item]:
-    logger.info(f"User message: {user_message}")
+@app.post("/chat/stream", response_class=EventSourceResponse)
+async def sse_items(user_message: UserMessage) -> AsyncIterable[ServerSentEvent]:
+    logger.info(f"User message: {user_message.message}")
+    yield ServerSentEvent(event="AI", data="__START__")
     async for chunk in chat_response(
         messages=[
             {
                 "role": "user",
-                "content": user_message,
+                "content": user_message.message,
             }
         ]
     ):
         if config.mock_response:
             await asyncio.sleep(0.2)
-        yield Item(name="AI", description=chunk)
+        yield ServerSentEvent(event="AI", data=chunk)
+    yield ServerSentEvent(event="AI", data="__END__")
